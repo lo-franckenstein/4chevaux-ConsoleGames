fa56f8d4fac3add4e0a52c4f5f2e32083421d383
Francken Louis <220153@CCM-Asty-Moulin.local>
2023-04-28 16:15:02 +0200

    Ajout d'animation

---
+++ Code/Code général/4Chevaux/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Threading;
+
+namespace _4Chevaux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////                                                                         /////////////////////////
+            /////////////////////////                          ZONE VARIABLE                                  /////////////////////////
+            /////////////////////////                                                                         /////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+            appel mp = new appel();
+            ConsoleKeyInfo keyLetter;
+            int nbrJoueur;
+
+
+
+
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////                                                                         /////////////////////////
+            /////////////////////////                          INTRODUCTION                                   /////////////////////////
+            /////////////////////////                                                                         /////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            mp.animation();
+            Thread.Sleep(200);
+            Console.ForegroundColor = ConsoleColor.White;
+            Thread.Sleep(500);
+            Console.WriteLine("WHAT'S UP!");
+            Thread.Sleep(250);
+            Console.WriteLine("Vous êtes ici pour jouer aux 4 chevaux, alors allons directement au but et commençons donc!\n");
+            Thread.Sleep(250);
+            Console.WriteLine("Connaissez-vous les règles du jeu ? (O = Oui | N = Non");
+            do
+            {
+                keyLetter = Console.ReadKey();
+            } while (keyLetter.Key != ConsoleKey.N && keyLetter.Key != ConsoleKey.O);
+
+            Console.Clear();
+            
+            if (keyLetter.Key == ConsoleKey.N)
+            {
+                Console.WriteLine("Les règles sont très simples ! \n");
+                Thread.Sleep(500);
+                Console.WriteLine("Chaque joueur (Jusque 4 joueurs) possède une écurie (Bleu - Rouge - Vert - Jaune). ");
+                Console.WriteLine("Et votre but consiste à ce que vos 4 chevaux qui se retrouvent au début à l'écurie (Il faut faire 6 pour les faire sortir de l'écurie et les placer au début du long chemin)");
+                Console.WriteLine("font le tour du plateau en suivant les chemins et en arrivant dans votre couloir et finir sur la dernière case.");
+                Console.WriteLine("Quand un cheval arrive sur une case déja occupée par un de ses chevaux,");
+                Console.WriteLine("alors il gagne une case en plus tandis que si c'est un cheval qui ne vous appartient pas, vous pouvez renvoyer le cheval dans son écurie et désormais votre cheval vole sa place.");
+                Thread.Sleep(500);
+                Console.WriteLine("Appuyez sur enter pour continuer...");
+                do
+                {
+                    keyLetter = Console.ReadKey();
+                } while (keyLetter.Key != ConsoleKey.Enter && keyLetter.Key != ConsoleKey.D0 && keyLetter.Key != ConsoleKey.NumPad0);
+            }
+
+
+            Thread.Sleep(250);
+            Console.Clear();
+            Thread.Sleep(250);
+            Console.WriteLine("Combien de joueurs êtes-vous en tout? (2 à 4 joueurs");
+            do
+            {
+                keyLetter = Console.ReadKey();
+            } while (keyLetter.Key != ConsoleKey.D2 && keyLetter.Key != ConsoleKey.NumPad2 && keyLetter.Key != ConsoleKey.D3 && keyLetter.Key != ConsoleKey.NumPad3 && keyLetter.Key != ConsoleKey.D4 && keyLetter.Key != ConsoleKey.NumPad4);
+            Console.Clear();
+            Thread.Sleep(500);
+            if(keyLetter.Key == ConsoleKey.D2 || keyLetter.Key == ConsoleKey.NumPad2)
+            {
+                nbrJoueur = 2;
+                Console.WriteLine("Voici les joueurs: \n");
+                Thread.Sleep(200);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[+] Joueur 1: Ecurie Rouge");
+                Thread.Sleep(200);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("[+] Joueur 2: Ecurie Jaune");
+            } else if(keyLetter.Key == ConsoleKey.D3 || keyLetter.Key == ConsoleKey.NumPad3) {
+                nbrJoueur = 3;
+                Console.WriteLine("Voici les joueurs: \n");
+                Thread.Sleep(200);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[+] Joueur 1: Ecurie Rouge");
+                Thread.Sleep(200);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("[+] Joueur 2: Ecurie Jaune");
+                Thread.Sleep(200);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("[+] Joueur 3: Ecurie Verte");
+            } else
+            {
+                nbrJoueur = 4;
+                Console.WriteLine("Voici les joueurs: \n");
+                Thread.Sleep(200);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[+] Joueur 1: Ecurie Rouge");
+                Thread.Sleep(200);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("[+] Joueur 2: Ecurie Jaune");
+                Thread.Sleep(200);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("[+] Joueur 3: Ecurie Verte");
+                Thread.Sleep(200);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("[+] Joueur 4: Ecurie Bleu");
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ Code/Code général/4Chevaux/appel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace _4Chevaux
+{
+    class appel
+    {
+        public void animation()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("      d8888         .d8888b.  888    888 8888888888 888     888        d8888 888     888 Y88b   d88P");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("     d8P888        d88P  Y88b 888    888 888        888     888       d88888 888     888  Y88b d88P  ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("    d8P 888        888    888 888    888 888        888     888      d88P888 888     888   Y88o88P   ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("   d8P  888        888        8888888888 8888888    Y88b   d88P     d88P 888 888     888    Y888P    ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("  d88   888        888        888    888 888         Y88b d88P     d88P  888 888     888    d888b    ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("  8888888888       888    888 888    888 888          Y88o88P     d88P   888 888     888   d88888b   ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("        888        Y88b  d88P 888    888 888           Y888P     d8888888888 Y88b. .d88P  d88P Y88b  ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("        888         'Y8888P'  888    888 8888888888     Y8P     d88P     888  'Y88888P'  d88P   Y88b");
+                Console.ForegroundColor = ConsoleColor.White;
+                Thread.Sleep(100);
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("      d8888         .d8888b.  888    888 8888888888 888     888        d8888 888     888 Y88b   d88P");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("     d8P888        d88P  Y88b 888    888 888        888     888       d88888 888     888  Y88b d88P  ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("    d8P 888        888    888 888    888 888        888     888      d88P888 888     888   Y88o88P   ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("   d8P  888        888        8888888888 8888888    Y88b   d88P     d88P 888 888     888    Y888P    ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("  d88   888        888        888    888 888         Y88b d88P     d88P  888 888     888    d888b    ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("  8888888888       888    888 888    888 888          Y88o88P     d88P   888 888     888   d88888b   ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("        888        Y88b  d88P 888    888 888           Y888P     d8888888888 Y88b. .d88P  d88P Y88b  ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("        888         'Y8888P'  888    888 8888888888     Y8P     d88P     888  'Y88888P'  d88P   Y88b");
+                Console.ForegroundColor = ConsoleColor.White;
+                Thread.Sleep(100);
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("      d8888         .d8888b.  888    888 8888888888 888     888        d8888 888     888 Y88b   d88P");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("     d8P888        d88P  Y88b 888    888 888        888     888       d88888 888     888  Y88b d88P  ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("    d8P 888        888    888 888    888 888        888     888      d88P888 888     888   Y88o88P   ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("   d8P  888        888        8888888888 8888888    Y88b   d88P     d88P 888 888     888    Y888P    ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("  d88   888        888        888    888 888         Y88b d88P     d88P  888 888     888    d888b    ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("  8888888888       888    888 888    888 888          Y88o88P     d88P   888 888     888   d88888b   ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("        888        Y88b  d88P 888    888 888           Y888P     d8888888888 Y88b. .d88P  d88P Y88b  ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("        888         'Y8888P'  888    888 8888888888     Y8P     d88P     888  'Y88888P'  d88P   Y88b");
+                Console.ForegroundColor = ConsoleColor.White;
+                Thread.Sleep(100);
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("      d8888         .d8888b.  888    888 8888888888 888     888        d8888 888     888 Y88b   d88P");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("     d8P888        d88P  Y88b 888    888 888        888     888       d88888 888     888  Y88b d88P  ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("    d8P 888        888    888 888    888 888        888     888      d88P888 888     888   Y88o88P   ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("   d8P  888        888        8888888888 8888888    Y88b   d88P     d88P 888 888     888    Y888P    ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("  d88   888        888        888    888 888         Y88b d88P     d88P  888 888     888    d888b    ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("  8888888888       888    888 888    888 888          Y88o88P     d88P   888 888     888   d88888b   ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("        888        Y88b  d88P 888    888 888           Y888P     d8888888888 Y88b. .d88P  d88P Y88b  ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("        888         'Y8888P'  888    888 8888888888     Y8P     d88P     888  'Y88888P'  d88P   Y88b");
+                Console.ForegroundColor = ConsoleColor.White;
+                Thread.Sleep(100);
+                Console.Clear();
+            }
+            
+
+        }
+    }
+}
